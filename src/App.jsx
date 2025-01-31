e3b62c9a9ca2402d38b0d3e4154e7501d0705905
mdshakil-ctg <developer.shakil.ctg@gmail.com>
2025-01-31 22:33:44 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+
+  return (
+    <>
+      <div className='bg-amber-300 text-center text-3xl'>
+        <h1>Hello World</h1>
+        <p>How was the day?</p>
+      </div>
+    </>
+  )
+}
+
+export default App
