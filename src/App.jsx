3928d55973af2f0aeaa58d3f52f6053180bc1f65
mdshakil-ctg <developer.shakil.ctg@gmail.com>
2025-01-31 23:18:42 +0600

    develop branch create and first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,7 @@ function App() {
   return (
     <>
       <div className='bg-amber-300 text-center text-3xl'>
-        <p>This is navbar content</p>
-        <h1>Hello World</h1>
-        <p>How was the day?</p>
-        <p>check the git command for automatic update for vercel</p>
-        <p>this is new line for check the feature branches update</p>
-        <p>add text for feature branch</p>
+        <p>Simple Todo App</p>
       </div>
     </>
   )
