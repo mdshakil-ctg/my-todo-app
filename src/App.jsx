7650a47109347c6d61ab43305155599a3d4f42c8
mdshakil-ctg <developer.shakil.ctg@gmail.com>
2025-01-31 22:54:37 +0600

    responsive navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <div className='bg-amber-300 text-center text-3xl'>
+        <p>This is navbar content</p>
         <h1>Hello World</h1>
         <p>How was the day?</p>
         <p>check the git command for automatic update for vercel</p>
